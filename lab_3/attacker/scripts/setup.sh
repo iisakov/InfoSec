e759fc21f9490013017cc90f43f1f5e03ae57557
iisakov <isakov.arh@yandex.ru>
2026-01-18 17:57:00 +0300

    fix req

--- lab_3/attacker/scripts/setup.sh
+++ lab_3/attacker/scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Установка необходимых пакетов
-apt update && apt install -y vim nmap jq metasploit-framework netcat-traditional telnet smbclient redis-tools python3-pip python3.13-venv curl wget python3 python3-pip awscli openssh-client hydra python2.7 python2.7-dev git gcc file openjdk-11-jre
+apt update && apt install -y vim nmap jq metasploit-framework netcat-traditional telnet smbclient redis-tools python3-pip python3.13-venv curl wget python3 python3-pip awscli openssh-client hydra git gcc file openjdk-11-jre
